2e1232fcd95e603e5fbcbb201e3710c5f372c8e3
Ivander <ivandersalvadorruiz@gmail.com>
2020-11-09 13:04:46 -0300

    Primeira alteração

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,8 @@
 import { useState } from 'react';
 
-function Contador() {
-  const [contador, setContador] = useState(1);
-
-  function adicionarContador() {
-    setContador(contador + 1);
-  }
-
-  return (
-    <>
-      <div>{contador}</div>
-      <button type="button" onClick={() => adicionarContador()}>Adicionar</button>
-    </>
-  )
-
-}
-
 function Home() {
   return (
-    <>
-      <div>
-        Home
-      </div>
-      <Contador />
-    </>
+    <h1>Home</h1>
   )
 }
 
